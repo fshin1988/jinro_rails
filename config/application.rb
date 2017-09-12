3788581a971c1ec289d12cd477e7f6c03aa215f9
fshin1988 <noalv88@gmail.com>
2017-09-13 00:22:47 +0900

    Add config of slim, rspec and so on

--- config/application.rb
+++ config/application.rb
@@ -11,5 +11,9 @@ module JinroRails
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
+    config.generators do |g|
+      g.template_engine = :slim
+      g.test_framework :rspec, view_specs: false, routing_specs: false
+    end
   end
 end
