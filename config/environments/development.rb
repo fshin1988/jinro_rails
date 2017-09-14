cd699d5608275a2377594c4e510c1c0b490369c3
fshin1988 <noalv88@gmail.com>
2017-09-14 21:52:58 +0900

    Add devise

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -59,4 +59,6 @@ Rails.application.configure do
     Bullet.console = true
     Bullet.rails_logger = true
   end
+
+  config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }
 end
