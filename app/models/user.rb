b31a8403a6f2afbf2753f37bc1d160f660b3fdda
fshin1988 <noalv88@gmail.com>
2017-09-24 22:24:12 +0900

    Add username to registaration page

--- app/models/user.rb
+++ app/models/user.rb
@@ -34,4 +34,7 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable,
          :confirmable
+
+  validates :username, presence: true
+
 end
