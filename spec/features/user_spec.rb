e95e16e4f9217c6d50c91d0b1b1f0563ca7e363b
fshin1988 <noalv88@gmail.com>
2017-09-29 20:50:53 +0900

    Add feature spec of user

---
+++ spec/features/user_spec.rb
@@ -0,0 +1,20 @@
+require 'rails_helper'
+
+describe 'User', type: :feature do
+  subject { page }
+
+  context 'when not logged in' do
+    it 'sign up' do
+      expect {
+        visit root_path
+        click_on 'サインアップ'
+        fill_in 'Email', with: 'test@example.co.jp'
+        fill_in 'Username', with: 'test_user'
+        fill_in 'Password', with: 'test1234'
+        fill_in 'Password confirmation', with: 'test1234'
+        click_on 'Sign up'
+      }.to change(User, :count).by(1)
+      expect(current_path).to eq root_path
+    end
+  end
+end
