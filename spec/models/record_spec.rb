3c14edf2a2cfe0ae61d29444c90d173bbbf42b26
fshin1988 <noalv88@gmail.com>
2017-10-07 13:46:59 +0900

    Remove redundant data definition from record spec

--- spec/models/record_spec.rb
+++ spec/models/record_spec.rb
@@ -1,9 +1,7 @@
 require 'rails_helper'
 
 RSpec.describe Record, type: :model do
-  let(:user) { create(:user) }
-  let(:player) { create(:player, user: user) }
-  let(:record) { create(:record, player: player, vote_target: player) }
+  let(:record) { create(:record) }
   it 'has a valid factory' do
     expect(record).to be_valid
   end
