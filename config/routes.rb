5f384869c877f12962bef28abe1a9692c2e884e1
fshin1988 <noalv88@gmail.com>
2017-10-12 23:09:05 +0900

    Add villages to routes

--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,6 @@ Rails.application.routes.draw do
   root to: 'welcome#index'
 
   devise_for :users
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+  resources :villages
 end
