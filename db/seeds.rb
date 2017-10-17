9c33f997705d0ed99612ed344ec61e23c0bb4d73
fshin1988 <noalv88@gmail.com>
2017-10-17 23:48:35 +0900

    Add data to seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,16 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+# Admin User
+admin = FactoryGirl.create(:confirmed_user, role: :admin, email: 'admin@example.co.jp', username: 'admin')
+
+# Village
+village = FactoryGirl.create(:village, user: admin, player_num: 13,
+                                       day: 0, start_time: Time.now + 1.hours, status: :not_started)
+
+# Room
+FactoryGirl.create(:room, village: village, room_type: :for_all)
+FactoryGirl.create(:room, village: village, room_type: :for_wolf)
+
+# Normal User
+13.times do
+  user = FactoryGirl.create(:user)
+  FactoryGirl.create(:player, user: user, village: village)
+end
