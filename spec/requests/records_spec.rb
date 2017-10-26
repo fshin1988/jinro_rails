bbd906254aa829a8d30d9fc841e2a7a7ff5e4e29
fshin1988 <noalv88@gmail.com>
2017-10-26 22:09:31 +0900

    Fix record spec

--- spec/requests/records_spec.rb
+++ spec/requests/records_spec.rb
@@ -1,9 +1,9 @@
 require 'rails_helper'
 
 RSpec.describe "Records", type: :request do
-  describe "GET /records" do
+  describe "GET /api/v1/records" do
     it "works! (now write some real specs)" do
-      get records_path
+      get api_v1_records_path
       expect(response).to have_http_status(200)
     end
   end
