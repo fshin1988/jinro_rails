9361007578a98fdbc054562805ee43402c6d1944
fshin1988 <noalv88@gmail.com>
2017-11-22 23:54:12 +0900

    Add title to room

--- app/helpers/rooms_helper.rb
+++ app/helpers/rooms_helper.rb
@@ -1,2 +1,13 @@
 module RoomsHelper
+  def room_title(village)
+    "#{village.name} #{room_day(village.day)}"
+  end
+
+  def room_day(day)
+    if day == 0
+      "プロローグ"
+    else
+      "#{day}日目"
+    end
+  end
 end
