e6750affed650994d82c4af875789e4c867b7a0e
fshin1988 <noalv88@gmail.com>
2017-11-25 20:41:10 +0900

    Display alert message if api returns error

--- app/javascript/packs/rooms/show.js
+++ app/javascript/packs/rooms/show.js
@@ -28,12 +28,31 @@ var noticeComponent = Vue.extend({
     }
 });
 
+var alertComponent = Vue.extend({
+    props: ['alertMessage'],
+    template: '\
+      <div class="alert alert-danger">\
+        <button type="button" class="close" data-dismiss="alert" aria-label="Close" v-on:click="emitReset">\
+          <span aria-hidden="true">&times;</span>\
+        </button>\
+        {{alertMessage}}\
+      </div>\
+    ',
+    methods: {
+      emitReset: function() {
+        this.$emit('reset')
+      }
+    }
+});
+
 new Vue({
   el: '#room-root',
   data: {
     chatDisplay: true,
     noticeDisplay: false,
     noticeMessage: "",
+    alertDisplay: false,
+    alertMessage: "",
     voteSelected: "",
     attackSelected: "",
     divineSelected: "",
@@ -48,25 +67,31 @@ new Vue({
         .then(res => {
           this.setNotice("投票先をセットしました")
         }, (error) => {
-          this.setError("エラーが発生しました")
+          this.setAlert("エラーが発生しました")
         });
     },
     setAttackTarget(id) {
       axios.put('/api/v1/records/' + id, { record: { attack_target_id: this.attackSelected } })
         .then(res => {
           this.setNotice("襲撃先をセットしました")
+        }, (error) => {
+          this.setAlert("エラーが発生しました")
         });
     },
     setDivineTarget(id) {
       axios.put('/api/v1/records/' + id, { record: { divine_target_id: this.divineSelected } })
         .then(res => {
           this.setNotice("占い先をセットしました")
+        }, (error) => {
+          this.setAlert("エラーが発生しました")
         });
     },
     setGuardTarget(id) {
       axios.put('/api/v1/records/' + id, { record: { guard_target_id: this.guardSelected } })
         .then(res => {
           this.setNotice("護衛先をセットしました")
+        }, (error) => {
+          this.setAlert("エラーが発生しました")
         });
     },
     switchArea: function() {
@@ -79,6 +104,13 @@ new Vue({
       this.noticeDisplay = true
       this.noticeMessage = message
     },
+    resetAlert: function() {
+      this.alertDisplay = false
+    },
+    setAlert: function(message) {
+      this.alertDisplay = true
+      this.alertMessage = message
+    },
     setInitialValue: function() {
       this.voteSelected = document.getElementById('vote-initial').getAttribute('initial')
       this.attackSelected = document.getElementById('attack-initial').getAttribute('initial')
@@ -88,6 +120,7 @@ new Vue({
   },
   components: {
     'switch-component': switchComponent,
-    'notice-component': noticeComponent
+    'notice-component': noticeComponent,
+    'alert-component': alertComponent
   }
 });
