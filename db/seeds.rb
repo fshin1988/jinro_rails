5b05e7352cf2b67988dc28a6864ff76dd8c349e0
fshin1988 <noalv88@gmail.com>
2017-11-28 00:18:31 +0900

    Don't send email in rake db:seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,12 +1,12 @@
 # Admin User
-admin = FactoryGirl.create(:confirmed_user, role: :admin, email: 'admin@example.co.jp', username: 'admin')
+admin = FactoryGirl.create(:user, role: :admin, email: 'admin@example.co.jp', username: 'admin', confirmed_at: Time.now)
 
 # Village
-village = FactoryGirl.create(:village, user: admin, player_num: 13,
+village = FactoryGirl.create(:village, user: admin, player_num: 5,
                                        day: 0, status: :not_started)
 
 # Normal User
-13.times do
-  user = FactoryGirl.create(:confirmed_user)
+5.times do
+  user = FactoryGirl.create(:user, confirmed_at: Time.now)
   FactoryGirl.create(:player, user: user, village: village)
 end
