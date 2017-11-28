05a1a784c080c476ebe8ab9dffb7a53c7a996e8d
fshin1988 <noalv88@gmail.com>
2017-11-28 23:39:53 +0900

    Change player_num of seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -2,11 +2,11 @@
 admin = FactoryGirl.create(:user, role: :admin, email: 'admin@example.co.jp', username: 'admin', confirmed_at: Time.now)
 
 # Village
-village = FactoryGirl.create(:village, user: admin, player_num: 5,
+village = FactoryGirl.create(:village, user: admin, player_num: 13,
                                        day: 0, status: :not_started)
 
 # Normal User
-5.times do
+13.times do
   user = FactoryGirl.create(:user, confirmed_at: Time.now)
   FactoryGirl.create(:player, user: user, village: village)
 end
