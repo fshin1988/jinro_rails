e7adcecf9f6636dea2a61383fdc1aeeaf3a28583
fshin1988 <noalv88@gmail.com>
2017-12-17 21:34:04 +0900

    Add policy for speak

--- app/policies/room_policy.rb
+++ app/policies/room_policy.rb
@@ -13,4 +13,24 @@ class RoomPolicy < ApplicationPolicy
       true
     end
   end
+
+  def speak?
+    if record.for_wolf?
+      case record.village.status
+      when 'in_play'
+        record.village.player_from_user(user)&.werewolf? && record.village.player_from_user(user)&.alive?
+      else
+        false
+      end
+    else
+      case record.village.status
+      when 'not_started', 'ended'
+        record.village.player_from_user(user)
+      when 'in_play'
+        record.village.player_from_user(user)&.alive?
+      else
+        false
+      end
+    end
+  end
 end
