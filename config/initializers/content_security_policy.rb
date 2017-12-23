b27072226c573677af194c923953a189329ea475
fshin1988 <noalv88@gmail.com>
2017-12-23 22:38:06 +0900

    Add rule to content_security_policy to use action cable

--- config/initializers/content_security_policy.rb
+++ config/initializers/content_security_policy.rb
@@ -9,6 +9,7 @@ Rails.application.config.content_security_policy do |p|
   p.object_src  :none
   p.script_src  :self, :https
   p.style_src   :self, :https, :unsafe_inline
+  p.connect_src :self, :https, 'ws://localhost:3000'
 
   # Specify URI for violation reports
   # p.report_uri "/csp-violation-report-endpoint"
