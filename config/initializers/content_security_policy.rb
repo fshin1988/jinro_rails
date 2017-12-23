a95e5c87ddf8e7571b9ecec60476f9ba022af4f6
fshin1988 <noalv88@gmail.com>
2017-12-23 23:03:52 +0900

    Don't use content_security_policy

--- config/initializers/content_security_policy.rb
+++ config/initializers/content_security_policy.rb
@@ -3,13 +3,13 @@
 # https://developer.mozilla.org/en-US/docs/Web/HTTP/Headers/Content-Security-Policy
 
 Rails.application.config.content_security_policy do |p|
-  p.default_src :self, :https
-  p.font_src    :self, :https, :data
-  p.img_src     :self, :https, :data
-  p.object_src  :none
-  p.script_src  :self, :https
-  p.style_src   :self, :https, :unsafe_inline
-  p.connect_src :self, :https, 'ws://localhost:3000'
+  # p.default_src :self, :https
+  # p.font_src    :self, :https, :data
+  # p.img_src     :self, :https, :data
+  # p.object_src  :none
+  # p.script_src  :self, :https
+  # p.style_src   :self, :https, :unsafe_inline
+  # p.connect_src :self, :https, 'ws://localhost:3000'
 
   # Specify URI for violation reports
   # p.report_uri "/csp-violation-report-endpoint"
