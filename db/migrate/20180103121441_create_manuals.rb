f21dc884f67da461a9cdab43148a108c2f7ab437
fshin1988 <noalv88@gmail.com>
2018-01-03 21:31:25 +0900

    Execute db:migrate

--- db/migrate/20180103121441_create_manuals.rb
+++ db/migrate/20180103121441_create_manuals.rb
@@ -1,7 +1,7 @@
 class CreateManuals < ActiveRecord::Migration[5.2]
   def change
     create_table :manuals do |t|
-      t.text :content
+      t.text :content, null: false
 
       t.timestamps
     end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2017_12_31_045737) do
+ActiveRecord::Schema.define(version: 2018_01_03_121441) do
 
   create_table "active_storage_attachments", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "name", null: false
@@ -31,6 +31,12 @@ ActiveRecord::Schema.define(version: 2017_12_31_045737) do
     t.string "checksum", null: false
     t.datetime "created_at", null: false
     t.index ["key"], name: "index_active_storage_blobs_on_key", unique: true
+  end
+
+  create_table "manuals", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
+    t.text "content", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
   create_table "players", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
