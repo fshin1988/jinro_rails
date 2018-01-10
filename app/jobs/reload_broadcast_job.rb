15170c59ca626b914933d7a191fd465a171653a9
fshin1988 <noalv88@gmail.com>
2018-01-10 22:34:21 +0900

    Reload room_for_dead in reload_broadcast_job

--- app/jobs/reload_broadcast_job.rb
+++ app/jobs/reload_broadcast_job.rb
@@ -4,5 +4,6 @@ class ReloadBroadcastJob < ApplicationJob
   def perform(village)
     ActionCable.server.broadcast "room:room_channel_#{village.room_for_all.id}", reload: true
     ActionCable.server.broadcast "room:room_channel_#{village.room_for_wolf.id}", reload: true
+    ActionCable.server.broadcast "room:room_channel_#{village.room_for_dead.id}", reload: true
   end
 end
