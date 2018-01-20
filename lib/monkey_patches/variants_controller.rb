592f2599d7e70a7997702470dd556eb7a34743d0
fshin1988 <noalv88@gmail.com>
2018-01-20 17:54:34 +0900

    Write info to log for debug

--- lib/monkey_patches/variants_controller.rb
+++ lib/monkey_patches/variants_controller.rb
@@ -1,6 +1,7 @@
 class ActiveStorage::VariantsController < ActionController::Base
   def show
     if blob = ActiveStorage::Blob.find_signed(params[:signed_blob_id])
+      logger.info "Using customized VariantsController"
       expires_in 120.minutes
       redirect_to ActiveStorage::Variant.new(blob, params[:variation_key]).processed.service_url(expires_in: 120.minutes, disposition: params[:disposition])
     else
