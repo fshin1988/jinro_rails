0e080175e05a2c2e4440fda25647cdf01356aaf7
fshin1988 <noalv88@gmail.com>
2018-01-20 18:12:43 +0900

    Move monkey_patches/variants_controller.rb to initializers

--- config/initializers/monkey_patches.rb
+++ config/initializers/monkey_patches.rb
@@ -1,4 +1,4 @@
-Dir[Rails.root.join('lib/monkey_patches/**/*.rb')].sort.each do |file|
-  Rails.logger.info "Reading initializers/monkey_patches.rb"
-  require file
-end
+# Dir[Rails.root.join('lib/monkey_patches/**/*.rb')].sort.each do |file|
+#   Rails.logger.info "Reading initializers/monkey_patches.rb"
+#   require file
+# end
