37a8f8e4373871dd06921e9d98e51c1c82c8061b
fshin1988 <noalv88@gmail.com>
2018-01-21 13:04:19 +0900

    Add spec of avatar_image_src

--- spec/models/player_spec.rb
+++ spec/models/player_spec.rb
@@ -19,4 +19,19 @@ RSpec.describe Player, type: :model do
       expect(player.human?).to be_falsey
     end
   end
+
+  describe '#avatar_image_src' do
+    context 'when a user does not have an avatar' do
+      it 'returns nil' do
+        expect(player.avatar_image_src).to be_nil
+      end
+    end
+
+    context 'when a user has a avatar' do
+      it 'returns url of avatar' do
+        player.user.avatar.attach(io: File.open("#{Rails.root}/spec/factories/data/logo.png"), filename: "logo.png")
+        expect(player.avatar_image_src =~ %r{^http://.*/rails/active_storage/variants/.*/logo\.png$}).not_to be_nil
+      end
+    end
+  end
 end
