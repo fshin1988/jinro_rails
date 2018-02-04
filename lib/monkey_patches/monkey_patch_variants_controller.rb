d19332a120befbe0ea186ac40dd93c58abbc8e2c
fshin1988 <noalv88@gmail.com>
2018-02-04 21:17:08 +0900

    Wait until UploadVariantAvatarJob finishes in variants_controller

--- lib/monkey_patches/monkey_patch_variants_controller.rb
+++ lib/monkey_patches/monkey_patch_variants_controller.rb
@@ -2,7 +2,12 @@ module ExtendVariantsController
   def show
     if blob = ActiveStorage::Blob.find_signed(params[:signed_blob_id])
       expires_in 120.minutes
-      redirect_to ActiveStorage::Variant.new(blob, params[:variation_key]).processed.service_url(expires_in: 120.minutes, disposition: params[:disposition])
+      variant = ActiveStorage::Variant.new(blob, params[:variation_key])
+      # Wait until UploadVariantAvatarJob finishes
+      until variant.send(:processed?)
+        sleep 1
+      end
+      redirect_to variant.service_url(expires_in: 120.minutes, disposition: params[:disposition])
     else
       head :not_found
     end
