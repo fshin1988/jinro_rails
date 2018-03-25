5f389bc8c8360a62725306ea967348e33f7e0259
fshin1988 <noalv88@gmail.com>
2018-03-25 20:21:49 +0900

    Rescue from ActiveStorage::InvariableError

--- app/jobs/upload_variant_avatar_job.rb
+++ app/jobs/upload_variant_avatar_job.rb
@@ -1,6 +1,10 @@
 class UploadVariantAvatarJob < ApplicationJob
   queue_as :default
 
+  rescue_from(ActiveStorage::InvariableError) do
+    nil
+  end
+
   def perform(owner)
     owner.avatar.variant(resize: "100x100").processed
   end
