b187ea8f3186ff057cf6278bb8ee11e810e4f2d3
Tomoyo <ksyrkt@hotmail.co.jp>
2019-04-12 19:22:26 +0900

    Add validation for duplicative post

--- app/models/post.rb
+++ app/models/post.rb
@@ -14,6 +14,7 @@
 
 class Post < ApplicationRecord
   before_validation :compress_content_size, if: :player?
+  before_validation :prohibit_duplicates, if: :player?
   after_create_commit { MessageBroadcastJob.perform_later self }
 
   enum owner: {
@@ -26,9 +27,17 @@ class Post < ApplicationRecord
 
   validates :player_id, presence: true, if: :player?
   validates :content, presence: true
+  validate :prohibit_duplicates
   validates :day, presence: true
 
   private
+
+  def prohibit_duplicates
+    lastpost = Post.where(player_id: player_id).where(room_id: room_id).last
+    if lastpost && lastpost.content == content
+        @errors.add(:base, '連続投稿はできません')
+    end
+  end
 
   def compress_content_size
     self.content = content[0, 500] if content.size > 500
