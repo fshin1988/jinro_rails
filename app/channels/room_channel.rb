63693212b08ba7ee4f94a95b4fd4a397319c4ceb
Shinya Fujita <noalv88@gmail.com>
2019-09-08 21:36:56 +0900

    Add API to post a message (#46)
    * Fix style of catching error on axios
    
    * Use api to post a message

--- app/channels/room_channel.rb
+++ app/channels/room_channel.rb
@@ -1,14 +1,6 @@
 class RoomChannel < ApplicationCable::Channel
-  include Pundit
-
   def subscribed
     stream_from "room:room_channel_#{params[:room_id]}"
     @room = Room.find params[:room_id]
-  end
-
-  def speak(data)
-    authorize @room, :speak?
-    player = @room.village.player_from_user(current_user)
-    @room.posts.create!(player: player, content: data['message'], day: @room.village.day)
   end
 end
