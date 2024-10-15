aee16bca02da3b167a6898f09b55577101e32a55
fshin1988 <noalv88@gmail.com>
2024-10-15 21:51:13 +0900

    Fix bug in reload_broadcast

--- app/jobs/reload_broadcast_job.rb
+++ app/jobs/reload_broadcast_job.rb
@@ -2,8 +2,8 @@ class ReloadBroadcastJob < ApplicationJob
   queue_as :default
 
   def perform(village)
-    ActionCable.server.broadcast "room:room_channel_#{village.room_for_all.id}", reload: true
-    ActionCable.server.broadcast "room:room_channel_#{village.room_for_wolf.id}", reload: true
-    ActionCable.server.broadcast "room:room_channel_#{village.room_for_dead.id}", reload: true
+    ActionCable.server.broadcast "room:room_channel_#{village.room_for_all.id}", { reload: true  }
+    ActionCable.server.broadcast "room:room_channel_#{village.room_for_wolf.id}", { reload: true }
+    ActionCable.server.broadcast "room:room_channel_#{village.room_for_dead.id}", { reload: true }
   end
 end
